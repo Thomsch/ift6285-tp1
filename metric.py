561011bb92fe6ddf8a80a5da1f7ed51a403d4dbf
Thomsch <thms.sch@gmail.com>
2018-02-24 17:24:03 -0500

    Added BLEU metrics

---
+++ metric.py
@@ -0,0 +1,23 @@
+import nltk.translate.bleu_score as nltk
+
+
+def bleu_sentence(reference, translation):
+    """
+    Wrapper for blue_sentence from NLTK.
+    :param reference: The reference sentence as a string.
+    :param translation: the translation sentence as a string.
+    :return: The BLEU score for the sentence
+    """
+    return nltk.sentence_bleu([reference.split()], translation.split())
+
+
+def bleu_corpus(references, translations):
+    """
+    Wrapper for bleu_corpus from NLTK.
+    :param references: The list of reference sentences as strings.
+    :param translations: The list of translated sentences as strings.
+    :return: The BLEU score for the corpus
+    """
+    references = [[sentence.split()] for sentence in references]
+    translations = [sentence.split() for sentence in translations]
+    return nltk.corpus_bleu(references, translations)
