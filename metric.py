49bc0a144c4b29a421ef3c7fe22ef79b2346d213
Thomsch <thms.sch@gmail.com>
2018-03-03 13:56:24 -0500

    Added accuracy metric from LSTM model

--- metric.py
+++ metric.py
@@ -23,3 +23,30 @@ def bleu_corpus(references, translations):
     references = [[sentence.split()] for sentence in references]
     translations = [sentence.split() for sentence in translations]
     return nltk.corpus_bleu(references, translations, WEIGHTS)
+
+
+def accuracy(references, translations):
+    """
+    Computes the word to word ratio between references and translation of correct translations.
+    If there is more or less words in the translation, they are counted as false.
+    :param references: The list of reference sentences as strings.
+    :param translations: The list of translated sentences as strings.
+    :return: The percentage of correctly translated word on the total number of words in the reference sentence.
+    """
+    test_acc_count, count_total = count_accuracy(references, translations)
+    return test_acc_count/count_total
+
+
+def count_accuracy(pred_sents,target_sents):
+    count_accu=0
+    total=0
+    for pred_sent,target_sent in zip(pred_sents,target_sents):
+        print(pred_sent)
+        print(target_sent)
+        pred_list=pred_sent.split(' ')
+        targ_list=target_sent[2:].split(' ')
+        for pred_token,target_token in zip(pred_list,targ_list):
+            total+=1
+            if pred_token==target_token:
+                count_accu+=1
+    return count_accu, total
