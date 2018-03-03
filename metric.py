3ae6dafb3e525d162e9147f9e9e4dd2f2f3952a1
Thomsch <thms.sch@gmail.com>
2018-03-03 16:38:45 -0500

    Implemented accuracy metric

--- metric.py
+++ metric.py
@@ -27,26 +27,38 @@ def bleu_corpus(references, translations):
 
 def accuracy(references, translations):
     """
-    Computes the word to word ratio between references and translation of correct translations.
-    If there is more or less words in the translation, they are counted as false.
+    Computes the average accuracy per sentence.
     :param references: The list of reference sentences as strings.
     :param translations: The list of translated sentences as strings.
-    :return: The percentage of correctly translated word on the total number of words in the reference sentence.
+    :return: The average accuracy per sentence.
+    :raise ValueError: If ``references`` and ``translations`` do not have the same length.
     """
-    test_acc_count, count_total = count_accuracy(references, translations)
-    return test_acc_count/count_total
+    if len(references) != len(translations):
+        raise ValueError("Lists must have the same length.")
+
+    accuracies = []
+    for reference, translation in zip(references, translations):
+        accuracies.append(sentence_accuracy(reference, translation))
+    return sum(accuracies) / len(accuracies)
 
 
-def count_accuracy(pred_sents,target_sents):
-    count_accu=0
-    total=0
-    for pred_sent,target_sent in zip(pred_sents,target_sents):
-        print(pred_sent)
-        print(target_sent)
-        pred_list=pred_sent.split(' ')
-        targ_list=target_sent[2:].split(' ')
-        for pred_token,target_token in zip(pred_list,targ_list):
-            total+=1
-            if pred_token==target_token:
-                count_accu+=1
-    return count_accu, total
+def sentence_accuracy(reference_sentence, translated_sentence):
+    """
+    Computes the ratio of correctly translated words in a reference sentence.
+    If the translation contains more or less words than the reference, they are counted as ``false``.
+    :param reference_sentence: The reference sentence as a string
+    :param translated_sentence: The translation sentence as a string
+    :return: the accuracy for a sentence.
+    """
+    correct = 0
+    total = 0
+
+    references = reference_sentence.split()
+    translations = translated_sentence.split()
+    for reference, translation in zip(references, translations):
+        if reference == translation:
+            correct += 1
+        total += 1
+
+    total += abs(len(references) - len(translations))
+    return correct / total
