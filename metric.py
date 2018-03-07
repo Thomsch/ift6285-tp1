655ac8385e43536637853a98e10b3e2209b0db26
Thomsch <thms.sch@gmail.com>
2018-03-07 11:12:56 -0500

    Fixed variable shadow

--- metric.py
+++ metric.py
@@ -64,10 +64,10 @@ def sentence_accuracy(reference_sentence, translated_sentence):
     return correct / total
 
 
-def display_result(test_name, sentences_number, accuracy, bleu_score):
+def display_result(test_name, sentences_number, accuracy_score, bleu_score):
     print(test_name)
     print("=========================")
     print("# sentence: {}".format(sentences_number))
-    print("Exactitude: {}".format(accuracy))
+    print("Exactitude: {}".format(accuracy_score))
     print("BLEU score: {}".format(bleu_score))
     print()
