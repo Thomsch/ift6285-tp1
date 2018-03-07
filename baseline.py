4c00f88bbc181fe31f10325055f4119e25026d3f
Thomsch <thms.sch@gmail.com>
2018-03-07 11:10:10 -0500

    Refactored signature of display_result

--- baseline.py
+++ baseline.py
@@ -6,12 +6,12 @@ import loader
 import metric
 
 
-def display_result(test_name, sentences_number, bleu_score, accuracy):
+def display_result(test_name, sentences_number, accuracy, bleu_score):
     print(test_name)
     print("=========================")
     print("# sentence: {}".format(sentences_number))
-    print("BLEU score: {}".format(bleu_score))
     print("Exactitude: {}".format(accuracy))
+    print("BLEU score: {}".format(bleu_score))
     print()
 
 
@@ -20,9 +20,9 @@ FILES_LARGE_CORPUS = 300
 x, y = loader.load("data/test")
 bleu = metric.bleu_corpus(y, x)
 accuracy = metric.accuracy(y, x)
-display_result("Test corpus", len(x), bleu, accuracy)
+display_result("Test corpus", len(x), accuracy, bleu)
 
 x, y = loader.load("data/train", FILES_LARGE_CORPUS)
 bleu = metric.bleu_corpus(y, x)
 accuracy = metric.accuracy(y, x)
-display_result("Training corpus - {} files".format(FILES_LARGE_CORPUS), len(x), bleu, accuracy)
+display_result("Training corpus - {} files".format(FILES_LARGE_CORPUS), len(x), accuracy, bleu)
